b38e6ec4a868d9fe0b4f41a53b472ee25c409482
rex <tesillio@gmail.com>
2017-06-19 19:28:03 +0900

    template chapter

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { TodosComponent } from './todos/todos.component';
 import { TodoComponent } from './todos/todo/todo.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, FormsModule
   ],
   declarations: [TodosComponent, TodoComponent],
   exports: [TodosComponent]
